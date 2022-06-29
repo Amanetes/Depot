d4a894da428836810495bfecca846715d495aed9
Sergey <ser_8904@mail.ru>
2022-06-29 13:23:13 +0300

    Validation!

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_29_062459) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_29_091400) do
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_29_062459) do
     t.decimal "price", precision: 8, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["title"], name: "index_products_on_title", unique: true
   end
 
 end
