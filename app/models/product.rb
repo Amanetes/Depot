ff36d80f68f5e901a55d28cdaa9628c903b45a20
Sergey <ser_8904@mail.ru>
2022-09-03 12:50:44 +0300

    Task B: Validation and Unit Testing

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,13 @@
+# frozen_string_literal: true
+
 class Product < ApplicationRecord
+  validates :title, :description, :image_url, presence: true
+  # БД хранит значение только с 2мя знаками после запятой
+  validates :title, uniqueness: true, length: { minimum: 10 }
+  # allow_blank игнорирует валидацию если стоит вместе с presence: true
+  validates :image_url, allow_blank: true, format: {
+    with: /\.(gif|jpg|png)\z/i,
+    message: 'must be a URL for GIF, JPG or PNG image.'
+  }
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,59 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+  test 'product attributes must not be empty' do
+    product = Product.new
+    assert_predicate product, :invalid?
+    assert_predicate product.errors[:title], :any?
+    assert_predicate product.errors[:description], :any?
+    assert_predicate product.errors[:price], :any?
+    assert_predicate product.errors[:image_url], :any?
+  end
+
+  test 'product price must be positive' do
+    product = Product.new(title: 'My Book Title',
+                          description: 'yyy',
+                          image_url: 'zzz.jpg')
+    product.price = -1
+    assert_predicate product, :invalid?
+    assert_equal ['must be greater than or equal to 0.01'], product.errors[:price]
+    product.price = 0
+    assert_predicate product, :invalid?
+    assert_equal ['must be greater than or equal to 0.01'], product.errors[:price]
+    product.price = 1
+    assert_predicate product, :valid?
+  end
+
+  def new_product(image_url)
+    Product.new(title: 'My Book Title',
+                description: 'yyy',
+                price: 1,
+                image_url:)
+  end
+
+  test 'image url' do
+    ok = %w[fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif]
+    bad = %w[fred.doc fred.gif/more fred.gif.more]
+
+    ok.each do |image_url|
+      assert_predicate new_product(image_url), :valid?
+      "#{image_url} must be valid"
+    end
+    bad.each do |image_url|
+      assert_predicate new_product(image_url), :invalid?
+      "#{image_url} must be invalid"
+    end
+  end
+
+  test 'product is not valid without a unique title - i18n' do
+    product = Product.new(title: products(:ruby).title,
+                          description: 'yyy',
+                          price: 1,
+                          image_url: 'fred.gif')
+    assert_predicate product, :invalid?
+    assert_equal [I18n.t('errors.messages.taken')], product.errors[:title]
+  end
 end
